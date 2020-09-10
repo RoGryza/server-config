ae08d27458c07a61082bd0c234c55e5bfcfac3d7
Rodrigo Gryzinski <rogryza@rogryza.com>
2020-09-10 05:04:06 -0300

    add local-persist and gluster

--- packer/main.pkr.hcl
+++ packer/main.pkr.hcl
@@ -31,14 +31,42 @@ build {
       "apt-get update",
       "apt-get upgrade -y",
       "apt-get install -y apt-transport-https docker ca-certificates unattended-upgrades ntp",
+      "add-apt-repository -y ppa:gluster/glusterfs-8",
+      "apt-get update",
+      "apt-get install -y glusterfs-server",
+      "systemctl enable glusterd --now",
     ]
   }
   provisioner "file" {
     destination = "/etc/apt/apt.conf.d/20auto-upgrades"
     source      = "20auto-upgrades"
   }
+
   provisioner "file" {
     destination = "/etc/ntp.conf"
     source      = "ntp.conf"
+  }
+
+  provisioner "file" {
+    destination = "/etc/systemd/system/docker-volume-local-persist.service"
+    source = "docker-local-persist/docker-volume-local-persist.service"
+  }
+
+  provisioner "file" {
+    destination = "/tmp/docker-volume-local-persist-checksum.txt"
+    source = "docker-local-persist/checksum.txt"
+  }
+
+  provisioner "shell" {
+    inline = [
+      "systemctl stop docker-volume-local-persist || true",
+      "if [ ! $(sha256sum -c /tmp/docker-volume-local-persist-checksum.txt) ]; then",
+      "wget https://github.com/MatchbookLab/local-persist/releases/download/v1.3.0/local-persist-linux-amd64 -O /usr/local/bin/docker-volume-local-persist",
+      "sha256sum -c /tmp/docker-volume-local-persist-checksum.txt",
+      "chmod +x /usr/local/bin/docker-volume-local-persist",
+      "fi",
+      "systemctl daemon-reload",
+      "systemctl enable docker-volume-local-persist --now",
+    ]
   }
 }
--- terraform/glusterfs.tf
+++ terraform/glusterfs.tf
@@ -1,131 +1,182 @@
-// resource "hcloud_volume" "node_shared_storage" {
-//   count    = var.node_count
-//   name     = "node-shared-storage${count.index}"
-//   location = hcloud_server.nodes[count.index].location
-//   size     = 10
-//   format   = "xfs"
-// }
+resource "hcloud_volume" "manager_brick" {
+  count    = var.manager_count
+  name     = "node-shared-storage-${count.index}"
+  location = hcloud_server.managers[count.index].location
+  size     = 10
+  format   = "xfs"
+}
 
-// resource "hcloud_volume_attachment" "node_shared_storage" {
-//   count     = var.node_count
-//   server_id = hcloud_server.nodes[count.index].id
-//   volume_id = hcloud_volume.node_shared_storage[count.index].id
-// }
+resource "hcloud_volume_attachment" "manager_brick" {
+  count     = var.manager_count
+  server_id = hcloud_server.managers[count.index].id
+  volume_id = hcloud_volume.manager_brick[count.index].id
+}
 
-// resource "null_resource" "setup_gluster" {
-//   count = var.node_count
-//   triggers = {
-//     server : hcloud_volume_attachment.node_shared_storage[count.index].server_id,
-//     volume : hcloud_volume.node_shared_storage[count.index].linux_device,
-//   }
+resource "null_resource" "manager_mount_brick" {
+  count = var.manager_count
+  triggers = {
+    server : hcloud_volume_attachment.manager_brick[count.index].server_id,
+    linux_device : hcloud_volume.manager_brick[count.index].linux_device,
+    connection_host : hcloud_server.managers[count.index].ipv4_address,
+    connection_port : random_integer.ssh_port.result,
+    connection_private_key : tls_private_key.terraform.private_key_pem,
+  }
 
-//   connection {
-//     host        = hcloud_server.nodes[count.index].ipv4_address
-//     port        = random_integer.ssh_port.result
-//     user        = "terraform"
-//     private_key = tls_private_key.terraform.private_key_pem
-//   }
+  connection {
+    host        = self.triggers.connection_host
+    port        = self.triggers.connection_port
+    user        = "terraform"
+    private_key = self.triggers.connection_private_key
+  }
 
-//   provisioner "remote-exec" {
-//     inline = [
-//       "sudo mkdir -p /export",
-//       "echo '${hcloud_volume.node_shared_storage[count.index].linux_device} /export xfs defaults 1 2' | sudo tee -a /etc/fstab",
-//       "sudo mount -a && sudo mount",
-//       "sudo mkdir -p /export/brick",
-//       "until $(which gluster >/dev/null 2>&1); do sleep 1; done",
-//       "until $(sudo systemctl enable --now glusterd >/dev/null 2>&1); do sleep 1; done",
-//     ]
-//   }
-// }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkdir -p /gluster",
+      "echo '${self.triggers.linux_device} /gluster xfs defaults 1 2' | sudo tee -a /etc/fstab",
+      "sudo mount -a && sudo mount",
+    ]
+  }
 
-// resource "null_resource" "gluster_probe_first" {
-//   depends_on = [null_resource.setup_gluster]
-//   triggers = {
-//     gluster : null_resource.setup_gluster.0.id,
-//     network : hcloud_server_network.private.0.ip,
-//   }
+  provisioner "remote-exec" {
+      when = destroy
+      inline = [
+          "sudo umount /gluster",
+          "sudo sed -i '/^${replace(self.triggers.linux_device, "/", "\\/")}/d' /etc/fstab",
+          "sudo rm -rf /gluster",
+      ]
+  }
+}
 
-//   connection {
-//     type        = "ssh"
-//     host        = hcloud_server.nodes.0.ipv4_address
-//     port        = random_integer.ssh_port.result
-//     user        = "terraform"
-//     private_key = tls_private_key.terraform.private_key_pem
-//   }
+resource "null_resource" "gluster_probe_first" {
+  triggers = {
+    brick : null_resource.manager_mount_brick.0.id,
+    network : join(", ", hcloud_server_network.private.*.ip),
+  }
 
-//   provisioner "remote-exec" {
-//     inline = [
-//       for i in range(1, var.node_count) :
-//       "sudo gluster peer probe '${hcloud_server_network.private[i].ip}'"
-//     ]
-//   }
-// }
+  connection {
+    host        = hcloud_server.managers.0.ipv4_address
+    port        = random_integer.ssh_port.result
+    user        = "terraform"
+    private_key = tls_private_key.terraform.private_key_pem
+  }
 
-// resource "null_resource" "gluster_probe_rest" {
-//   count      = var.node_count - 1
-//   depends_on = [null_resource.gluster_probe_first]
-//   triggers = {
-//     gluster : null_resource.gluster_probe_first.id,
-//     network : hcloud_server_network.private.0.ip,
-//   }
+  provisioner "remote-exec" {
+    inline = [
+      for i in range(1, var.manager_count) :
+      "sudo gluster peer probe '${hcloud_server_network.private[i].ip}'"
+    ]
+  }
+}
 
-//   connection {
-//     host        = hcloud_server.nodes[count.index + 1].ipv4_address
-//     port        = random_integer.ssh_port.result
-//     user        = "terraform"
-//     private_key = tls_private_key.terraform.private_key_pem
-//   }
+resource "null_resource" "gluster_probe_rest" {
+  count      = var.manager_count - 1
+  triggers = {
+    gluster : null_resource.gluster_probe_first.id,
+  }
 
-//   provisioner "remote-exec" {
-//     inline = ["sudo gluster peer probe '${hcloud_server_network.private.0.ip}'"]
-//   }
-// }
+  connection {
+    host        = hcloud_server.managers[count.index + 1].ipv4_address
+    port        = random_integer.ssh_port.result
+    user        = "terraform"
+    private_key = tls_private_key.terraform.private_key_pem
+  }
 
-// resource "null_resource" "gluster_shared_volume" {
-//   depends_on = [null_resource.gluster_probe_rest]
-//   triggers = {
-//     gluster : join(", ", null_resource.gluster_probe_rest.*.id)
-//   }
+  provisioner "remote-exec" {
+    inline = ["sudo gluster peer probe '${hcloud_server_network.private.0.ip}'"]
+  }
+}
 
-//   connection {
-//     type        = "ssh"
-//     host        = hcloud_server.nodes.0.ipv4_address
-//     port        = random_integer.ssh_port.result
-//     user        = "terraform"
-//     private_key = tls_private_key.terraform.private_key_pem
-//   }
+resource "null_resource" "gluster_detach" {
+    count = var.manager_count - 1
+    depends_on = [null_resource.gluster_probe_rest]
+    triggers = {
+        host_ip : hcloud_server_network.private[count.index + 1].ip,
+        connection_host : hcloud_server.managers.0.ipv4_address,
+        connection_port : random_integer.ssh_port.result,
+        connection_private_key : tls_private_key.terraform.private_key_pem,
+    }
+    lifecycle {
+        ignore_changes = [triggers]
+    }
 
-//   provisioner "remote-exec" {
-//     inline = [
-//       join(" ", concat(
-//         ["sudo gluster volume create gv0 replica ${var.node_count}"],
-//         [for net in hcloud_server_network.private : "${net.ip}:/export/brick"]
-//       )),
-//       "sudo gluster volume start gv0",
-//     ]
-//   }
-// }
+  connection {
+    host        = self.triggers.connection_host
+    port        = self.triggers.connection_port
+    user        = "terraform"
+    private_key = self.triggers.connection_private_key
+  }
 
-// resource "null_resource" "gluster_shared_volume_mount" {
-//   count      = var.node_count
-//   depends_on = [null_resource.gluster_shared_volume]
-//   triggers = {
-//     gluster : null_resource.gluster_shared_volume.id
-//   }
+  // TODO this doesn't work, peering should be handled better
+  provisioner "remote-exec" {
+      when = destroy
+      inline = ["sudo gluster peer detach ${self.triggers.host_ip}"]
+  }
+}
 
-//   connection {
-//     type        = "ssh"
-//     host        = hcloud_server.nodes[count.index].ipv4_address
-//     port        = random_integer.ssh_port.result
-//     user        = "terraform"
-//     private_key = tls_private_key.terraform.private_key_pem
-//   }
+resource "null_resource" "gluster_volume_gv0" {
+  triggers = {
+    gluster : join(", ", null_resource.gluster_probe_rest.*.id),
+    connection_host : hcloud_server.managers.0.ipv4_address,
+    connection_port : random_integer.ssh_port.result,
+    connection_private_key : tls_private_key.terraform.private_key_pem,
+  }
 
-//   provisioner "remote-exec" {
-//     inline = [
-//       "sudo mkdir -p /mnt/shared",
-//       "echo '${hcloud_server.nodes[count.index].name}:/gv0 /mnt/shared glusterfs defaults,_netdev,x-systemd.requires=gluster.service,x-systemd.automount 0 0' | sudo tee -a /etc/fstab",
-//       "sudo mount -a && sudo mount",
-//     ]
-//   }
-// }
+  connection {
+    host        = self.triggers.connection_host
+    port        = self.triggers.connection_port
+    user        = "terraform"
+    private_key = self.triggers.connection_private_key
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      join(" ", concat(
+        ["sudo gluster volume create gv0 replica ${var.manager_count}"],
+        [for net in hcloud_server_network.private : "${net.ip}:/gluster/brick"]
+      )),
+      "sudo gluster volume start gv0",
+    ]
+  }
+
+  provisioner "remote-exec" {
+      when = destroy
+      inline = [
+          "sudo gluster volume stop gv0",
+          "sudo gluster volume delete gv0",
+      ]
+  }
+}
+
+resource "null_resource" "mount_export" {
+  count      = var.manager_count
+  triggers = {
+    gluster : null_resource.gluster_volume_gv0.id,
+    connection_host : hcloud_server.managers[count.index].ipv4_address,
+    connection_port : random_integer.ssh_port.result,
+    connection_private_key : tls_private_key.terraform.private_key_pem,
+  }
+
+  connection {
+    host        = self.triggers.connection_host
+    port        = self.triggers.connection_port
+    user        = "terraform"
+    private_key = self.triggers.connection_private_key
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkdir -p /export",
+      "echo '${self.triggers.connection_host}:/gv0 /export glusterfs defaults,_netdev,x-systemd.requires=gluster.service,x-systemd.automount 0 0' | sudo tee -a /etc/fstab",
+      "sudo mount -a && sudo mount",
+    ]
+  }
+
+  provisioner "remote-exec" {
+      when = destroy
+      inline = [
+          "sudo umount /export",
+          "sudo sed -i '/^${replace(self.triggers.connection_host, "/", "\\/")}/d' /etc/fstab",
+          "sudo rm -rf /export",
+      ]
+  }
+}
