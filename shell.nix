82466a5a2dd974d1f7ba8beae8e515894c863efc
Rodrigo Gryzinski <rogryza@rogryza.com>
2020-11-08 16:52:03 -0300

    Prepare for nixops

--- shell.nix
+++ shell.nix
@@ -1,4 +1,15 @@
-{ pkgs ? import ./nix/nixpkgs.nix {} }:
+{ pkgs ? import ./nix/nixpkgs.nix {}, }:
+let
+  overrides = import ./overrides.nix { inherit pkgs; };
+in
 pkgs.mkShell {
-  buildInputs = [pkgs.niv pkgs.nixops];
+  buildInputs = with pkgs; [
+    niv
+    poetry
+    # TODO
+    # (pkgs.poetry2nix.mkPoetryEnv {
+    #   projectDir = ./.;
+    #   overrides = pkgs.poetry2nix.overrides.withDefaults overrides;
+    # })
+  ];
 }
