6e2695c2fb104cf032b8e15808795264d6b9e633
Rodrigo Gryzinski <rogryza@rogryza.com>
2020-11-11 21:21:41 -0300

    Use nixops_hcloud

--- rogryza.me.nix
+++ rogryza.me.nix
@@ -1,55 +1,65 @@
-{ host, password }:
+{ password }:
 {
   network.description = "rogryza.me";
-  main = { config, pkgs, ... }:
-  let
-  in {
-    deployment.targetHost = host;
+  resources.hcloudSshKeys.key = {
+    publicKey = builtins.readFile ./keys/id_rsa.pub;
+  };
+  resources.hcloudVolumes.storage = {
+    size = 10;
+    location = "hel1";
+  };
+  main = { pkgs, resources, ... }: {
+    deployment.targetEnv = "hcloud";
+    deployment.hcloud = {
+      serverType = "cx11";
+      location = "hel1";
+      sshKeys = [ resources.hcloudSshKeys.key ];
+      volumes = [
+        {
+          volume = resources.hcloudVolumes.storage;
+          mountPoint = "/storage";
+          fileSystem = {
+            fsType = "ext4";
+            autoFormat = true;
+          };
+        }
+      ];
+    };
 
     imports = [
       <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
-      ./modules/taskserver.nix
+      ./fetchHetznerKeys.nix
     ];
-    nix.maxJobs = pkgs.lib.mkDefault 1;
-    nix.trustedUsers = ["rogryza"];
-
-    boot.initrd.availableKernelModules = [ "ata_piix" "virtio_pci" "xhci_pci" "sd_mod" "sr_mod" ];
-    boot.initrd.kernelModules = [ ];
-    boot.kernelModules = [ ];
-    boot.extraModulePackages = [ ];
-    boot.loader.grub.enable = true;
-    boot.loader.grub.version = 2;
-    system.stateVersion = "20.03";
-    boot.loader.grub.devices = ["/dev/sda"];
-
-    fileSystems."/" = { device = "/dev/sda"; fsType = "ext4"; };
-    swapDevices = [ ];
+    nix.trustedUsers = [ "rogryza" ];
+    services.fetchHetznerKeys.enable = true;
 
     networking.hostName = "rogryza";
-    networking.interfaces.ens3.useDHCP = true;
-
     time.timeZone = "Europe/Amsterdam";
 
     services.openssh.enable = true;
     services.openssh.openFirewall = true;
     services.openssh.passwordAuthentication = false;
     # services.openssh.permitRootLogin = "no"; # TODO disable root login without breaking nixops
+    programs.mosh.enable = true;
 
     networking.firewall.enable = true;
 
+    users.mutableUsers = false;
     users.groups.wheel = {};
     # TODO change root to auto-generated keys
-    users.users.root.openssh.authorizedKeys.keyFiles = [./keys/id_rsa.pub];
+    # TODO change root password
+    users.users.root.password = password;
+    users.users.root.openssh.authorizedKeys.keyFiles = [ ./keys/id_rsa.pub ];
     deployment.keys.rogryza-passsword.text = password;
     users.groups.rogryza = {};
     users.users.rogryza = {
       isNormalUser = true;
       createHome = true;
       group = "rogryza";
-      extraGroups = ["wheel"];
+      extraGroups = [ "wheel" ];
       shell = pkgs.bash;
-      openssh.authorizedKeys.keyFiles = [./keys/id_rsa.pub];
-      passwordFile = "/run/keys/rogryza-password";
+      openssh.authorizedKeys.keyFiles = [ ./keys/id_rsa.pub ];
+      passwordFile = "/run/keys/rogryza-password"; # TODO this isn't working, maybe it's fetching the file from the local machine?
     };
   };
 }
