83a8c8fd869bad2741affbbac0d793647d1cc2eb
Rodrigo Gryzinski <contact@rogryza.me>
2021-07-04 11:48:35 -0300

    FreshRSS running

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,14 +1,26 @@
 provider "cloudflare" {
 }
 
+# TODO manage zone settings
 resource "cloudflare_zone" "rogryza_me" {
   zone = "rogryza.me"
 }
 
-resource "cloudflare_record" "rogryza_me" {
+resource "cloudflare_record" "rogryza_me_root" {
   zone_id = cloudflare_zone.rogryza_me.id
   name    = "@"
   type    = "A"
   value   = digitalocean_droplet.default.ipv4_address
-  proxied = true
+  # TODO should this be true?
+  proxied = false
+}
+
+# TODO maybe we could manage this from docker:
+# https://github.com/tiredofit/docker-traefik-cloudflare-companion
+resource "cloudflare_record" "rogryza_me_wildcard" {
+  zone_id = cloudflare_zone.rogryza_me.id
+  name    = "*"
+  type    = "CNAME"
+  value   = cloudflare_zone.rogryza_me.zone
+  proxied = false
 }
